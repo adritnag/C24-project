b44e135207985c264b0959ecda89e60383a997d6
adritnag <tanusreeroy467@gmail.com>
2020-05-05 21:10:19 +0530

    Created the Ground

--- js/Ground.js
+++ js/Ground.js
@@ -1,10 +1,16 @@
 class Ground {
-  constructor(x, y, width, height) {
+  constructor() {
     var options ={
       isStatic : true
     }
+    this.body = Bodies.rectangle(0,790,800,20,options);
+   
   };
 
   display(){
+    
+    rectMode(CENTER);
+    rect(400,790,800,20);
+    
   }
 }
